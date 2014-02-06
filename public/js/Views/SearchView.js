4fbc2d0156548aad9c890390bfbf20b7906dfebf
Luis Elizondo <lelizondo@gmail.com>
2014-02-06 00:04:48 -0600

    Add Morris.js and Raphael as dependencies; Fixed backbone search page

--- public/js/Views/SearchView.js
+++ public/js/Views/SearchView.js
@@ -1,9 +1,8 @@
 App.Views.SearchView = Backbone.View.extend({
-	// el: "#page-wrapper",
-	tagName: "tbody",
+	el: ".log-results",
+	tagName: "div",
+	template: _.template($(".logRow").html()),
 	initialize: function() {
-		console.log("Hola views");
-
 		this.render();
 	},
 	render: function() {
@@ -11,13 +10,8 @@ App.Views.SearchView = Backbone.View.extend({
 		var collection = new App.Collections.Logs();
 		collection.fetch({
 			success: function(col, response) {
-				console.log(col);
-				console.log(response);
-
 				_.each(response.results, function(result) {
-					var template = _.template($(".logRow").html());
-
-					self.$el.append(template, response);
+					self.$el.prepend(self.template(result));
 				})
 			},
 			error: function(col, error) {
@@ -25,5 +19,30 @@ App.Views.SearchView = Backbone.View.extend({
 				console.log(error);
 			}
 		});
+	}
+});
+
+App.Views.GraphView = Backbone.View.extend({
+	el: ".graph",
+	tagName: "div",
+	initialize: function(data) {
+		console.log(data);
+	},
+	render: function() {
+		// Morris.Bar({
+		//   element: 'bar-example',
+		//   data: [
+		//     { y: '2006', a: 100, b: 90 },
+		//     { y: '2007', a: 75,  b: 65 },
+		//     { y: '2008', a: 50,  b: 40 },
+		//     { y: '2009', a: 75,  b: 65 },
+		//     { y: '2010', a: 50,  b: 40 },
+		//     { y: '2011', a: 75,  b: 65 },
+		//     { y: '2012', a: 100, b: 90 }
+		//   ],
+		//   xkey: 'y',
+		//   ykeys: ['a', 'b'],
+		//   labels: ['Series A', 'Series B']
+		// });
 	}
 });
