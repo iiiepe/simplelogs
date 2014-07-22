24dbebe0da26554558b0f0e9049d0b90722dca5f
Luis Elizondo <lelizondo@gmail.com>
2014-07-22 16:46:50 -0500

    Add fig.yml file

--- start.js
+++ start.js
@@ -1,21 +1,25 @@
 var express = require('express');
 var http = require('http');
 var mongoose = require('mongoose');
-var app = require("./app");
+var include = require("includemvc");
+var app = include.app();
 var config = app.config;
 
 var server = http.createServer(app);
 var io = require("./lib/socket")(server);
 var fs = require("fs");
-mongoose.connect(config.mongodburi);
+
+if(config.mongodburi) {
+	mongoose.connect(config.mongodburi);
+}
 
 /** 
  * Load all modules enabled in config.modules
  * The module must have an index.js present 
  */
-var modules = config.modules;
-modules.forEach(function(module) {
-	app.use(require("./modules/" + module));
+var components = config.components;
+components.forEach(function(component) {
+	app.use(require("./components/" + component));
 });
 
 server.listen(app.get('port'), function(){
